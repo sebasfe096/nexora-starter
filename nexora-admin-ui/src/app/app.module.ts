00a83a628e1e74a1044e13e28509b3487727de6e
Sebastian Saavedra <fesa00c@tienlacecsc.onmicrosoft.>
2026-03-11 12:50:28 -0500

    chore(lint): add eslint and unused-imports plugin
    - Install 'eslint', '@eslint/js', and 'typescript-eslint' for code analysis.
    - Add 'eslint-plugin-unused-imports' to automate cleanup of unused imports.
    - Configure 'eslint.config.js' with standard Angular rules and import cleanup.

--- nexora-admin-ui/src/app/app.module.ts
+++ nexora-admin-ui/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,13 +15,7 @@ import { AuthInterceptor } from './core/interceptors/auth.interceptor';
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
