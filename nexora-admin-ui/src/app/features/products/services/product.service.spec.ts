727bd3a28266eafe212fc770fbb63ffdbe8cd7b0
Sebastian Saavedra <fesa00c@tienlacecsc.onmicrosoft.>
2026-03-12 09:37:46 -0500

    test(product-service): add unit tests for getProductById and createProduct
    - Implement test suite for 'ProductService' using HttpClientTestingModule.
    - Verify dynamic URL construction using environment-centralized configuration.
    - Ensure correct HTTP method usage and response handling for both GET and POST.

--- nexora-admin-ui/src/app/features/products/services/product.service.spec.ts
+++ nexora-admin-ui/src/app/features/products/services/product.service.spec.ts
@@ -1,8 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { ProductService } from './product.service';
+import {Product, ProductService} from './product.service';
+import {environment} from "../../../../environments/environment";
 
 describe('ProductService', () => {
+  const productURL = [
+    environment.API_DOMAIN,
+    environment.API_CONTEXT,
+    environment.SERVICES.PRODUCTS
+  ].join('');
   let service: ProductService;
   let httpMock: HttpTestingController;
 
@@ -26,9 +32,67 @@ describe('ProductService', () => {
       expect(response.length).toBe(1);
       expect(response[0].status).toBe('ACTIVE');
     });
-
-    const req = httpMock.expectOne('https://api-gw-node1.nexora.com/v1/products');
+    console.log("url" , productURL)
+    const req = httpMock.expectOne(productURL);
     expect(req.request.method).toBe('GET');
     req.flush(mockWrappedResponse);
+  });
+
+  it('should call new api kong URL to fetch getProductById', () => {
+    const productId = 1;
+    const mockProduct: Product = {
+      id: 1,
+      sku: "NEX12",
+      name: 'Widget Pro',
+      description: 'decorador',
+      price: 9000,
+      stock: 5,
+      category: 'Utilidades',
+      status: 'ACTIVE',
+      createdAt: '12-03-2026',
+    };
+
+    service.getProductById(productId).subscribe(response => {
+      expect(response).toEqual(mockProduct);
+      expect(response.id).toBe(productId);
+    });
+
+    const req = httpMock.expectOne(`${productURL}/${productId}`);
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockProduct);
+  });
+
+  it('should call new api kong URL to create a product', () => {
+    const newProduct: Partial<Product> = {
+      sku: "NEX12",
+      name: 'Widget Pro',
+      description: 'decorador',
+      price: 9000,
+      category: 'Utilidades'
+    };
+
+    const mockResponse =  {
+      id: 100,
+      sku: "NEX12",
+      name: 'Widget Pro',
+      description: 'decorador',
+      price: 9000,
+      category: 'Utilidades',
+      stock: 5,
+      status: 'ACTIVE' as 'ACTIVE' | 'INACTIVE' | 'DISCONTINUED',
+      createdAt: '12-03-2026'
+    } ;
+
+    service.createProduct(newProduct).subscribe(response => {
+      expect(response).toEqual(mockResponse);
+      expect(response.id).toBe(100);
+    });
+
+    const req = httpMock.expectOne(productURL);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(newProduct);
+
+    req.flush(mockResponse);
   });
 });
